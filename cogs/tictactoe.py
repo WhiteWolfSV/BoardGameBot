5e03e2d5469a3bdb65fc18512d7c21ac43ae17a1
Philip Mellbin <philip.mellbin@learnet.se>
2022-04-21 09:41:14 +0200

    Adding rows to tictactoe.py

--- cogs/tictactoe.py
+++ cogs/tictactoe.py
@@ -8,6 +8,9 @@ class TicTacToe(commands.Cog):
 
     @commands.command()
     async def ttt(self, ctx):
+        # Here it prints out the numbers
         w, h = 8, 5
         b = [["  " for x in range(w)] for y in range(h)]
-        await ctx.send(f"|{b[0][0]}|{b[1][0]}|{b[2][0]}|")
+        await ctx.send(f"|{b[1][1]}|{b[2][1]}|{b[3][1]}|") # Row 1
+        await ctx.send(f"|{b[1][2]}|{b[2][2]}|{b[3][2]}|") # Row 2
+        await ctx.send(f"|{b[1][3]}|{b[2][3]}|{b[3][3]}|") # Row 3
