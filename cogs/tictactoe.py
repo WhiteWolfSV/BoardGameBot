626a14f44171fe0edcff7643ae7c1f95adc28efa
Philip Mellbin <philip.mellbin@learnet.se>
2022-04-21 12:30:28 +0200

    added fill conditions

--- cogs/tictactoe.py
+++ cogs/tictactoe.py
@@ -8,24 +8,40 @@ class TicTacToe(commands.Cog):
 
     @commands.command()
     async def ttt(self, ctx):
-        # Here it prints out the numbers
-        w, h = 8, 5
+        w, h = 3, 3
         b = [["  " for x in range(w)] for y in range(h)]
 
         winner = 'none'
         player1 = 'X'
         player2 = 'O'
-        Run = True
+        curentplayer = player1
+        nextplayer = player2
 
-        await ctx.send(f"|{b[1][1]}|{b[2][1]}|{b[3][1]}|") # Row 1
-        await ctx.send("------------------------")
-        await ctx.send(f"|{b[1][2]}|{b[2][2]}|{b[3][2]}|") # Row 2
-        await ctx.send("------------------------")
-        await ctx.send(f"|{b[1][3]}|{b[2][3]}|{b[3][3]}|") # Row 3
+        Run = True
+        def printboard(b):
+            await ctx.send(f"|{b[0][0]}|{b[0][1]}|{b[0][2]}|"
+                           f"\n------------------------"
+                           f"\n|{b[1][0]}|{b[1][1]}|{b[1][2]}|"
+                           f"\n------------------------"
+                           f"|{b[1][0]}|{b[1][1]}|{b[1][2]}|\n")
+        printboard(b)
+
+        def fill(b):
+            await ctx.send(f"fill in box")
+            inpx = await ctx.send("row: ")
+            inpy = await ctx.send("column: ")
+
+            if inpx >= 1 and inpx <= 2 and b[inpx] != nextplayer:
+                if inpy >= 1 and inpx <=2 and b[inpy] != nextplayer:
+                    b[inpx][inpy] = [curentplayer][curentplayer]
+
+
 
         curentPlayer = player1
 
         await ctx.send(f"Player ones turn. comment _help for help to play")
+
+
 
     async def help(self, ctx):
         await ctx.send(f"to fill a box press x(column), y(row)")
