d62496c28a86af9f16be88aee6361bda40a87c5b
Philip Mellbin <philip.mellbin@learnet.se>
2022-04-21 11:30:41 +0200

    Added lines and help option

--- cogs/tictactoe.py
+++ cogs/tictactoe.py
@@ -11,6 +11,22 @@ class TicTacToe(commands.Cog):
         # Here it prints out the numbers
         w, h = 8, 5
         b = [["  " for x in range(w)] for y in range(h)]
+
+        winner = 'none'
+        player1 = 'X'
+        player2 = 'O'
+        Run = True
+
         await ctx.send(f"|{b[1][1]}|{b[2][1]}|{b[3][1]}|") # Row 1
+        await ctx.send("------------------------")
         await ctx.send(f"|{b[1][2]}|{b[2][2]}|{b[3][2]}|") # Row 2
+        await ctx.send("------------------------")
         await ctx.send(f"|{b[1][3]}|{b[2][3]}|{b[3][3]}|") # Row 3
+
+        curentPlayer = player1
+
+        await ctx.send(f"Player ones turn. comment _help for help to play")
+
+    async def help(self, ctx):
+        await ctx.send(f"to fill a box press x(column), y(row)")
+        await ctx.send(f"for instance: 2, 2 for center")
