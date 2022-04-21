d5cbb301d2ac8cd03c14de09dffa27ba87d3d7ff
Theodor <what112222@gmail.com>
2022-04-21 12:17:05 +0200

    Added a purge command

--- cogs/botcommands.py
+++ cogs/botcommands.py
@@ -16,6 +16,12 @@ class BotCommands(commands.Cog):
     async def pong(self, ctx):
         await ctx.send("ping!")
 
+    @commands.command()
+    async def purge(self, ctx, amount=3):
+        if str(botMasterRoleId) in str(ctx.message.author.roles):
+            await ctx.channel.purge(limit=amount+1)
+            await ctx.send(f'Successfully purged `{amount}` messages. Requested by **{ctx.message.author}**')
+
 
 def setup(bot):
     bot.add_cog(BotCommands(bot))
