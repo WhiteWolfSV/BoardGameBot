bb4c7a9d46b5ded608bad9fe6073915e37cfca4f
Theodor <what112222@gmail.com>
2022-04-26 19:21:07 +0200

    Added button support for command _hl

--- cogs/highlow.py
+++ cogs/highlow.py
@@ -1,6 +1,26 @@
 import discord
-from discord.ui import Button
+from discord.ui import Button, View
 from discord.ext import commands
+
+
+class BlueButton(Button):
+    def __init__(self, label):
+        super().__init__(label=label, style=discord.ButtonStyle.blurple)
+
+
+class GreenButton(Button):
+    def __init__(self, label):
+        super().__init__(label=label, style=discord.ButtonStyle.green)
+
+
+class RedButton(Button):
+    def __init__(self, label):
+        super().__init__(label=label, style=discord.ButtonStyle.red)
+
+
+class GrayButton(Button):
+    def __init__(self, label):
+        super().__init__(label=label, style=discord.ButtonStyle.gray)
 
 
 class HighLow(commands.Cog):
@@ -8,9 +28,20 @@ class HighLow(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def hl(self, ctx):
-        button = Button(style=discord.ButtonStyle.green)
-        await ctx.send("Test", components=[button])
+    async def hl(self, ctx, message="Default"):
+        view = View()
+        button = GrayButton(message)
+
+        async def button_callback(interaction):
+            button = GreenButton(message)
+            view.clear_items()
+            view.add_item(button)
+            await interaction.response.edit_message(view=view)
+
+        button.callback = button_callback
+
+        view.add_item(button)
+        await ctx.send("Test", view=view)
 
 
 async def setup(bot):
