daa18dd713f29b5e96bf834b7940a4e61738c34b
Philip Mellbin <philip.mellbin@learnet.se>
2022-05-01 15:56:49 +0200

    Third draft of tictactoe.py compleate

--- cogs/tictactoe.py
+++ cogs/tictactoe.py
@@ -1,11 +1,13 @@
 import discord
-import numpy
 from discord.ext import commands
-
+import random
 
 class TicTacToe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    global b
+    w, h = 3, 3
 
     @commands.command()
     async def ttt(self, ctx, p1: discord.Member, p2: discord.Member):
@@ -18,35 +20,49 @@ class TicTacToe(commands.Cog):
         global curentplayer
         global brd
         global b
-        player1 =p1
-        player2 = p2
-        curentplayer = player1
+        global count
+        curentplayer = " "
+        count = 0
         gameend = True
-
-
         if gameend:
-            w, h = 3, 3
-            b = [["Empty" for x in range(w)] for y in range(h)]
-            WinConditions = [b[0][0], b[0][1], b[0][2]], \
-                            [b[1][0], b[1][1], b[1][2]], \
-                            [b[2][0], b[2][1], b[2][2]], \
-                            [b[0][0], b[1][0], b[2][0]], \
-                            [b[0][1], b[1][1], b[2][1]], \
-                            [b[0][2], b[1][2], b[2][2]], \
-                            [b[0][0], b[1][1], b[2][2]], \
-                            [b[0][2], b[1][1], b[2][0]]
+            WinConditions = [0, 1, 2], \
+                            [3, 4, 5], \
+                            [6, 7, 8], \
+                            [0, 3, 6], \
+                            [1, 4, 7], \
+                            [2, 5, 8], \
+                            [0, 4, 8], \
+                            [2, 4, 6]
+
             global brd
-            brd = (f"|{b[0][0]}|{b[0][1]}|{b[0][2]}|"
-                   f"\n---------------"
-                   f"\n|{b[1][0]}|{b[1][1]}|{b[1][2]}|"
-                   f"\n---------------"
-                   f"|{b[2][0]}|{b[2][1]}|{b[2][2]}|\n")
-            await ctx.send(brd) #Print out the board
+            brd = ["T", "T", "T", # Board format
+                   "T", "T", "T",
+                   "T", "T", "T"]
             gameend = False
+            player1 = p1
+            player2 = p2
+
+            # Print out the board
+            line = ""
+            for x in range(len(brd)): # Print out a board
+                if x == 2 or x == 5 or x == 8:
+                    line += " " + brd[x]
+                    await ctx.send(line)
+                    line = ""
+                else:
+                    line += " " + brd[x]
+
+            num = random.randint(1, 3) #Randomly generates a number between 1 and 2 to decide who starts
+            if num == 1:
+                curentplayer = player1
+            elif num == 2:
+                curentplayer = player2
+            await ctx.send("* <@" + str(curentplayer.id) + "> Starts!")
+
         else:
             await ctx.send("Game is running")
     @commands.command()
-    async def fill(self, ctx, inpx: int, inpy: int):
+    async def fill(self, ctx, inp: int): #Fill comand
         global player1
         global player2
         global brd
@@ -54,6 +70,8 @@ class TicTacToe(commands.Cog):
         global gameend
         global curentplayer
         global b
+        global mark
+        global count
 
         if gameend == False:
             if curentplayer == ctx.author: #if it's the turn of the one using this comand
@@ -61,16 +79,19 @@ class TicTacToe(commands.Cog):
                     mark = "X"
                 else:
                     mark = "O"
-                if -1 < inpx < 3: #check if everything goes
-                    if -1 < inpy < 3:
-                        b[inpx][inpy] = mark #Problem with fill
-                        await ctx.send(brd)
-
-                    else:
-                        await ctx.send("Faulty input. Please try again(Use int between 0 and 2 for x and y)")
-                        await ctx.send(brd)
+                if 0 < inp < 10 and brd[inp - 1] == "T": #check if everything goes
+                         brd[inp - 1] = mark
+                         count = count + 1
+                         line = " "
+                         for x in range(len(brd)): # Print out the board again
+                             if x == 2 or x == 5 or x == 8:
+                                 line += " " + brd[x]
+                                 await ctx.send(line)
+                                 line = " "
+                             else:
+                                 line += " " + brd[x]
                 else:
-                    await ctx.send("Faulty input. Please try again(Use int between 0 and 2 for x and y)")
+                    await ctx.send("Faulty input. Please try again(Use int between 1 and 9)")
                     await ctx.send(brd)
             else:
                 await ctx.send("Please wait until it's your turn")
@@ -79,31 +100,25 @@ class TicTacToe(commands.Cog):
             await ctx.send("Start a new game to use this command")
             await ctx.send(brd)
 
-        if "" not in brd:
-            await ctx.send("Tie") #Check if it's a tie
+        if count == 9:
+            await ctx.send("It's a tie!")
             gameend = True
 
 
         def win(WinConditions, mark): #Checks if win conditions are fufilled
             for conditions in WinConditions:
-                if brd[0] == mark and brd[1] == mark and brd[2] == mark:
-                    Gameend = True
+                if brd[conditions[0]] == mark and brd[conditions[1]] == mark and brd[conditions[2]] == mark:
+                    gameend = True
+                    return gameend
 
-        win(WinConditions, mark)
+        win(WinConditions, mark) #Prints out the winner
         if gameend == True:
-            await ctx.send("Winner is: ", curentplayer)
+            await ctx.send("Winner: ", curentplayer)
 
-        if curentplayer == player1: #Switches player
+        if curentplayer == player1: # Switches player
             curentplayer = player2
         elif curentplayer == player2:
             curentplayer = player1
-
-    @commands.command()
-    async def tictactoe_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("please mention 2 players for this comand")
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send("Please make shure to mention players using @")
 
 
 def setup(bot):
